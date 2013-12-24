f2ec36b4c9eef22756ad0eedb793c0248fb482b5
James Ward <james@jamesward.org>
2013-12-24 13:12:28 -0700

    add concurrency examples

--- build.sbt
+++ build.sbt
@@ -5,7 +5,7 @@ version := "1.0"
 scalaVersion := "2.10.2"
 
 libraryDependencies ++= Seq(
-  // if you need scala xml, uncomment the following line
-  // "org.scala-lang" %% "scala-xml" % "2.11.0",
-  "org.scalatest" %% "scalatest" % "1.9.1" % "test"
+  "org.scalatest" %% "scalatest" % "1.9.1" % "test",
+  "net.databinder.dispatch" %% "dispatch-core" % "0.11.0", // async http library
+  "org.slf4j" % "slf4j-nop" % "1.6.4" // used by dispatch
 )
--- src/main/scala/Concurrency.scala
+++ src/main/scala/Concurrency.scala
@@ -1,15 +1,73 @@
 object Concurrency {
   def main(args: Array[String]): Unit = {
     
-    // Future & Promise
+    // Futures
+    import scala.concurrent.{Future, Await}
+    import scala.concurrent.duration.Duration
+    import scala.concurrent.ExecutionContext.global
     
-    // http client (dispatch)
+    val f: Future[String] = Future("hello, future")(global)
     
-    // resilience / failure handling
+    f.onSuccess {
+      case msg => println(msg)
+    } (global)
     
-    // composition (Future.seq, pipelines)
+    println(Await.result(f, Duration.Inf).toUpperCase) // block until the future completes
     
-    // mention actors (maybe a simple example)
+    
+    // Promises
+    import scala.concurrent.Promise
+
+    val p: Promise[String] = Promise[String]()
+
+    p.future.foreach(println)(global)
+    
+    p.success("promise fulfilled")
+    
+    println(Await.result(p.future, Duration.Inf).toUpperCase)
+    
+    
+    // Failure Handling
+    val promiseFailure = Promise[String]()
+    
+    val futureWithRecovery = promiseFailure.future.recover {
+      case e: RuntimeException => "error"
+    } (global)
+    
+    futureWithRecovery.foreach(println)(global)
+    
+    promiseFailure.failure(new RuntimeException())
+    
+    println(Await.result(futureWithRecovery, Duration.Inf).toUpperCase)
+    
+    
+    // Composition
+    
+    // Import the implicit global execution context under a different identifier since the
+    // non-implicit one was already imported under the global identifier
+    import scala.concurrent.ExecutionContext.Implicits.{global => ImplicitGlobalExecutionContext}
+    
+    val futureOne = Future.successful(1)
+    val futureTwo = Future.successful(2)
+    
+    val futureOneAndTwo = Future.sequence(Seq(futureOne, futureTwo))
+    
+    println(Await.result(futureOneAndTwo, Duration.Inf))
+    
+    val futureThree = for {
+      one <- futureOne
+      two <- futureTwo
+    } yield one + two
+    
+    println(Await.result(futureThree, Duration.Inf))
+    
+    
+    // Non-Blocking IO
+    import dispatch._
+    
+    val futureResponse = Http(url("http://www.scala-lang.org") OK as.String)
+    
+    println(Await.result(futureResponse, Duration.Inf).length)
     
   }
 }
