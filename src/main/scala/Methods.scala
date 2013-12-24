50cc89dc6ae843d923aba5885da2279852ff05b8
James Ward <james@jamesward.org>
2013-12-24 13:31:03 -0700

    added code examples for methods

--- src/main/scala/Methods.scala
+++ src/main/scala/Methods.scala
@@ -1,11 +1,43 @@
 object Methods {
   def main(args: Array[String]): Unit = {
     
-    // default values
+    // Inner Methods
     
-    // var args
+    def inside(foo: String): String = foo.toUpperCase
     
-    // named parameters
+    println(inside("foo"))
+    
+    
+    // Default Values
+    
+    def defaultFoo(foo: String = "hello"): String = inside(foo)
+    
+    println(defaultFoo())
+    
+    
+    // Multiple Values
+    
+    def multiValues(foos: String*): Int = foos.size
+    
+    println(multiValues("one"))
+    
+    println(multiValues("one", "two", "three"))
+    
+    println(multiValues("one"), multiValues("one", "two"))
+    
+    
+    // Parameter Sets
+    
+    def twoParameterSets(foo: String)(bar: Int): String = s"$bar:$foo"
+    
+    println(twoParameterSets("hello")(1))
+    
+    
+    // Named Parameters
+    
+    def namedParams(foo: Option[String] = None, bar: String) = bar + " " + foo.getOrElse("no foo")
+    
+    println(namedParams(bar = "something"))
     
   }
   
