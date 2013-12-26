637075f68bcaaccccad02a2a6f746008eb74dd6e
James Ward <james@jamesward.org>
2013-12-26 13:26:21 -0700

    code examples for implicits

--- src/main/scala/Implicits.scala
+++ src/main/scala/Implicits.scala
@@ -1,17 +1,49 @@
 object Implicits {
+
+  case class Bar(name: String)
+
+  implicit class RichBar(val self: Bar) extends AnyVal {
+    def prefixedName: String = s"Bar name: ${self.name}"
+  }
+  
   def main(args: Array[String]): Unit = {
     
-    // implicit parameters
+    // Implicit Parameters
+
+    def upperCaseBarName(implicit bar: Bar): Bar = bar.copy(bar.name.toUpperCase)
     
-    // future execution context
+    val explicitFooBar = upperCaseBarName(new Bar("explicit"))
     
-    // reference Play JSON
+    implicit val bar = new Bar("implicit")
     
-    // extension methods
+    val implicitFooBar = upperCaseBarName
     
-    // type classes
+    println(explicitFooBar, implicitFooBar)
     
-    // presedence rules with scoping
+    
+    // Extension Methods
+    
+    println(bar.prefixedName)
+    
+    
+    // Type Classes
+    
+    val bars = List(Bar("Beez"), Bar("All Star"), Bar("Cheese"))
+
+    val sortedBarNames = bars.map(_.name).sorted
+
+    println(sortedBarNames)
+    
+    trait BarOrdering extends scala.math.Ordering[Bar] {
+      def compare(x: Bar, y: Bar): Int = x.name.compare(y.name)
+    }
+
+    implicit object BarOrdering extends BarOrdering
+    
+    val sortedBars = bars.sorted
+    
+    println(sortedBars)
     
   }
+  
 }
