b849d54688b1cc1cbb7e3b66646b97ee6ae0e43a
James Ward <james@jamesward.org>
2013-12-26 16:16:42 -0700

    add examples for java interop

--- src/main/scala/JavaInterop.scala
+++ src/main/scala/JavaInterop.scala
@@ -1,13 +1,48 @@
 object JavaInterop {
   def main(args: Array[String]): Unit = {
     
-    // call java from scala
+    // Java Interop
     
-    // implement java interfaces, extends classes
+    val url = new java.net.URL("http://scala-lang.org")
     
-    // converters (to and from)
+    val uuid = java.util.UUID.randomUUID().toString
     
-    // extension methods
+    println(url, uuid)
+    
+    
+    // Implementing Java Interfaces
+    
+    class MyRunnable extends Runnable {
+      def run(): Unit = println("i am a runnable")
+    }
+    
+    new Thread(new MyRunnable).start()
+    
+    
+    // Extending Java Classes
+    
+    class MyThread extends Thread {
+      override def run(): Unit = println("i am a thread")
+    }
+    
+    (new MyThread).run()
+    
+    
+    // Collection Converters
+    
+    val javaList = new java.util.ArrayList[String]()
+    javaList.add("hello")
+    javaList.add("world")
+    
+    import scala.collection.JavaConverters._
+    
+    val scalaList: scala.collection.mutable.Buffer[String] = javaList.asScala.map(_.toUpperCase)
+    
+    println(s"scalaList = $scalaList")
+
+    val newJavaList: java.util.List[String] = scalaList.asJava
+    
+    println(s"newJavaList = $newJavaList")
     
   }
 }
