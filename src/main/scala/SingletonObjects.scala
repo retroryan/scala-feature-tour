25e45090567733cc38a7681000fbd0a4c65e78bc
James Ward <james@jamesward.org>
2013-12-26 18:17:52 -0700

    code examples for singletons

--- src/main/scala/SingletonObjects.scala
+++ src/main/scala/SingletonObjects.scala
@@ -1,14 +1,44 @@
-object SingletonObjects {
-  def main(args: Array[String]): Unit = {
+object SingletonObjects extends App {
+  
+  // Defining and Using Singleton Objects
+  
+  object Foo {
+    println("Foo Created")
     
-    // package objects
-    
-    // companion objects, apply methods
-    
-    // objects / singletons
-    
-    // implicit objects
-    
+    def sayHello() = println("hello")
   }
+  
+  Foo.sayHello()
+
+
+  // Package Objects
+  
+  import utils._
+  
+  println(prependHello(world))
+  
+  
+  // Companion Objects
+  
+  class Person {
+    override def toString: String = "Person"
+  }
+  
+  object Person {
+    def create(): Person = new Person
+  }
+  
+  println(Person.create())
+  
+  
+  // Case Class Companions
+  
+  case class Bar(name: String)
+  
+  val bar1 = Bar.apply("Beez")
+  
+  val bar2 = Bar("Neez")
+  
+  println(bar1, bar2)
   
 }
---
+++ src/main/scala/utils/package.scala
@@ -0,0 +1,7 @@
+package object utils {
+  
+  val world: String = "World"
+  
+  def prependHello(s: String): String = s"hello, $s"
+  
+}
