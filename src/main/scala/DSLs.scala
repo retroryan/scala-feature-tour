51affe7ca13d6575075f5265a31520c39b26ad7e
James Ward <james@jamesward.org>
2013-12-27 14:12:54 -0700

    code for dsls

--- src/main/scala/DSLs.scala
+++ src/main/scala/DSLs.scala
@@ -1,19 +1,63 @@
 object DSLs {
   def main(args: Array[String]): Unit = {
+
+    // Optional Parens
     
-    // language flexability
+    val hello: String = "hello"
     
-    // optional dots
+    val helloDoubleLength: Int = hello.length() + hello.length
     
-    // methods as operators
+    println(helloDoubleLength)
     
-    // method name flexability
     
-    // example ??? (maybe Complex Numbers)
+    // Optional Dots
     
-    // extension methods
+    val h: Char = hello.charAt(0)
     
-    // ScalaTest as an example
+    val e: Char = hello charAt 1
+    
+    println(h, e)
+    
+    val maxHE = hello.charAt(0).max(e)
+    
+    val minHE = hello charAt 0 min e
+    
+    println(maxHE, minHE)
+    
+    
+    // Symbolic Method Names / Operators
+    
+    def @!(c: Char): Char = if (c.isLower) c.toUpper else c.toLower
+    
+    val flipCaseA = @!('a')
+    
+    println(flipCaseA)
+    
+    
+    class MyString(s: String) {
+      def >!<(other: String): String = s.take(s.length / 2) + other.drop(other.length / 2)
+    }
+
+    val myHello = new MyString("hello")
+    
+    val help: String = myHello >!< "kelp"
+    
+    println(help)
+    
+    
+    // Right Associative Methods
+    
+    class FunString(s: String) {
+      def ~~:(c: Char): String = s.map(_.toString + c.toString).mkString
+    }
+    
+    val funHello = new FunString("hello")
+    
+    val normalWithDot = funHello.~~:('a')
+    
+    val reversedWithout = 'e' ~~: funHello
+    
+    println(normalWithDot, reversedWithout)
     
   }
   
