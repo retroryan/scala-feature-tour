cabeff0bc3b601d8a40e086abf881fa2ac7266d9
James Ward <james@jamesward.org>
2014-01-05 08:45:48 -0700

    remove isInstanceOf

--- src/main/scala/TypeInference.scala
+++ src/main/scala/TypeInference.scala
@@ -1,38 +1,40 @@
 object TypeInference {
   def main(args: Array[String]): Unit = {
+
+    // Methods Require Typed Parameters
+
+    def printAString(s: String): Unit = println(s)
+
+    def hello(s: String): String = s"hello, $s"
+
+    printAString(hello("world"))
+
+
+    // Method Return Types Can Be Inferred
+
+    def bye(s: String) = s"bye, $s"
+
+    printAString(bye("world"))
     
-    // Explicit Types
+    
+    // Explicit Value Types
     
     val foo: String = "foo"
     
-    println(foo.isInstanceOf[String])
+    printAString(foo)
     
     
     // Value Inference
     
     val bar = "bar"
     
-    println(bar.isInstanceOf[String])
+    printAString(bar)
     
     val listOfInt = List(1,2,3)
-    
-    println(listOfInt.isInstanceOf[List[Int]])
-    
-    
-    // Methods Require Typed Parameters
-    
-    def hello(s: String): String = s"hello, $s"
 
-    println(hello("world").isInstanceOf[String])
+    def printEachInt(l: List[Int]) = l.foreach(println)
     
-    
-    // Method Return Types Can Be Inferred
-    
-    def bye(s: String) = s"bye, $s"
-    
-    val byeWorld = bye("world")
-    
-    println(byeWorld.isInstanceOf[String])
+    printEachInt(listOfInt)
     
   }
   
