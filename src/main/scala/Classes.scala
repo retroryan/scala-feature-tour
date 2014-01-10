65ca537b3fb7cd5d6e5640987fd245ff7f564716
retroryan <ryan@grandcloud.com>
2014-01-09 18:18:49 -0700

    Initial Classes samples

--- src/main/scala/Classes.scala
+++ src/main/scala/Classes.scala
@@ -2,19 +2,25 @@ object Classes {
   def main(args: Array[String]): Unit = {
 
     // Defining Classes with Constructor Params
-    class Food(val calories:Int)
+    class Food(val calories: Int)
 
-    class Salad(val lettuceCalories:Int, val dressingCalories:Int) extends Food(lettuceCalories+dressingCalories)
+    class Salad(val lettuceCalories: Int, val dressingCalories: Int) extends Food(lettuceCalories + dressingCalories)
 
     // Overriding Methods with Values
-    class Menu(items:List[Food]) {
+    // Scala implements the uniform access principle - methods and variables can both be accessed the same
+    class Menu(items: List[Food]) {
       def numberOfMenuItems() = items.size
     }
 
-    //Dinners only consist of Salads
-    class Dinner(items:List[Salad]) extends Menu(items){
+    //Dinners only consists of Salads
+    class Dinner(items: List[Salad]) extends Menu(items) {
       override val numberOfMenuItems = 2 * items.size
     }
+
+    val s1 = new Salad(5,5)
+    val s2 = new Salad(15,15)
+    val dinner = new Dinner(List(s1,s2))
+    println(s"Number Of Menu Items = ${dinner.numberOfMenuItems}")
 
   }
 }
