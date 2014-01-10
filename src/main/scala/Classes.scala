e937ce4b143aaefdf1b4f7ce34f6c50afe9971cd
James Ward <james@jamesward.org>
2014-01-09 17:24:10 -0700

    added

---
+++ src/main/scala/Classes.scala
@@ -0,0 +1,14 @@
+object Classes {
+  def main(args: Array[String]): Unit = {
+
+    // Defining Classes
+    
+    
+    // Classes with Constructor Params
+    
+    
+    // Overriding Methods with Values
+    
+    
+  }
+}
