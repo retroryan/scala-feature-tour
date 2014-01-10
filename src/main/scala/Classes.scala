b096eaee341e0ec1da1f43b4a9453e093d29e565
retroryan <ryan@grandcloud.com>
2014-01-09 18:08:51 -0700

    Initial Classes samples

--- src/main/scala/Classes.scala
+++ src/main/scala/Classes.scala
@@ -1,14 +1,20 @@
 object Classes {
   def main(args: Array[String]): Unit = {
 
-    // Defining Classes
-    
-    
-    // Classes with Constructor Params
-    
-    
+    // Defining Classes with Constructor Params
+    class Food(val calories:Int)
+
+    class Salad(val lettuceCalories:Int, val dressingCalories:Int) extends Food(lettuceCalories+dressingCalories)
+
     // Overriding Methods with Values
-    
-    
+    class Menu(items:List[Food]) {
+      def numberOfMenuItems() = items.size
+    }
+
+    //Dinners only consist of Salads
+    class Dinner(items:List[Salad]) extends Menu(items){
+      override val numberOfMenuItems = 2 * items.size
+    }
+
   }
 }
